635283c2ef87358b03809e9c18079229b6c886cb
Oshprengel <oshprengel@gmail.com>
2021-09-17 11:48:07 -0600

    quiz is now fully functional

--- JS/script.js
+++ JS/script.js
@@ -2,19 +2,41 @@
 //Handles an artist submission
 $("#submitArtist").on('click', playQuiz);
 
-////////////////////////////////////////////////////////////////Global Event Functions////////////////////////////////////////////////////////////////////////////////
+//prevents multiple event listeners from being created on answers submit
+isFirst = true
 
 //Asks multiple questions about the artist within the text input at the time the function is called
 function playQuiz(){
+  ////////////////event handler for clicks within the answers div///////////////
+  $("#answers").on('click',(evt)=>{
+  switch(evt.target.nodeName){
+    case "INPUT":
+        currUserAnswer = evt.target
+        Array.from($("#answers input")).forEach((currEle)=>{
+        if (currEle != evt.target){
+        $(currEle).prop("checked", false)
+        }
+      })
+      break;
+    case "BUTTON":
+      takeScore(questionNo)
+      askQuestion(questionNo)
+    }
+  })
+//if a new artist is submitted mid quiz then question no is set to null to prevent the current quiz from interfering with the new one
+$("#submitArtist").on('click', ()=>{
+  questionNo = null;
+});
   ////////////////variables for artists data and players current score///////////////
-  userInput = $("#searchBar").val()
-  artistType = null;
-  artistOrigin = null;
-  artistStart = null;
-  artistRelease = null;
-  playerScore = 0;
-  gameOver = false;
-  artistName = null;
+  var userInput = $("#searchBar").val()
+  var artistOrigin = null;
+  var artistRelease = null;
+  var artistStartYr = null;
+  var artistType = null;
+  var playerScore = 0;
+  var artistName = null;
+  var questionNo = 0;
+  var currUserAnswer = null;
   ///////////////////Ajax call which assigns artist data to its proper variables///////////////////
   //formats the user input into the proper format
   userInput = userInput.replace(" ", "%20")
@@ -43,61 +65,81 @@ function playQuiz(){
   
   ///////////////////functions neccesary for playing the quiz///////////////////
   function askQuestion(questionNo){
-    //Creates a list of questions for a band 
-    const questionsBand = [
-      `When was ${artistName} formed?`,
-      `Where was ${artistName} formed?`,
+    
+    //Creates a list of questions
+    const questions = [
+      `When was ${artistName} ${artistType === 'Person' ? 'born' : 'formed'}?`,
+      `Where was ${artistName} ${artistType === 'Person' ? 'born' : 'formed'}?`,
       `Which one of these releases is from ${artistName}`
     ]
-    //creates a list of questions for a person
-    const questionsPerson = [];
-    questionsBand.forEach((currstring)=>{
-      questionsPerson.push(currstring.replace("formed","born")
-    )})
-    //asks a question(which question is asked is dependent on the questionNo var and the artistType)
-    if(questionNo === 0){
-      console.log(artistStartYr)
-      if (artistType === "Person"){
-        $("#question").html(questionsPerson[0])
-        appendAnswers(generateDates(artistStartYr,3),artistStartYr)
-      }
-      else{
-        $("#question").html(questionsBand[0])
-        appendAnswers(generateDates(artistStartYr,3),artistStartYr)
-      }
+    //random index var to be used for splicing answers into array
+    let randomIndex = 0;
+    //asks a question(which question is asked is dependent on the questionNo var)
+    switch(questionNo) {
+      case 0:
+        //append the question to html
+        $("#question").html(questions[0])
+        //generate an array of random dates then splice the correct answer into the array
+        const dates = generateDates(artistStartYr,3)
+        randomIndex = Math.floor(Math.random() * (dates.length + 1))
+        dates.splice(randomIndex,0,artistStartYr)
+        appendAnswers(dates)
+        break;
+      case 1:
+        $("#question").html(questions[1])
+        const places = generateLocations(artistOrigin,3)
+        randomIndex = Math.floor(Math.random() * (places.length + 1))
+        places.splice(randomIndex,0,artistOrigin)
+        appendAnswers(places)
+        break;
+      case 2:
+        $("#question").html(questions[2])
+        const albums = generateAlbums(artistRelease,3)
+        randomIndex = Math.floor(Math.random() * (albums.length + 1))
+        albums.splice(randomIndex,0,artistRelease)
+        appendAnswers(albums)
+        break;
+      case 3:
+        displayResults(3,playerScore)
     }
-    if(questionNo ===1){
-      if (artistType === "Person"){
-        $("#question").html(questionsPerson[1])
-        appendAnswers(generateLocations(),generateLocations(),generateLocations(),artistOrigin)
-      }
-      else{
-        $("#question").html(questionsBand[1])
-        appendAnswers(generateLocations(),generateLocations(),generateLocations(),artistOrigin)
-      }
-    }
-    if(questionNo ===2){
-      if (artistType === "Person"){
-        $("#question").html(questionsPerson[2])
-        appendAnswers(generateAlbums(),generateAlbums(),generateAlbums(),artistRelease)
-      }
-      else{
-        $("#question").html(questionsBand[2])
-        appendAnswers(generateAlbums(),generateAlbums(),generateAlbums(),artistRelease)
-      }
-    }
-    if(questionNo ===3){
-      displayResults(3,playerScore)
-    } 
   };
   //displays the users score using the playerScore var and the declares that the game is oever, takes in the total number of questions as param 1 and total right as param 2
   function displayResults(noQuestions, NoRight){
-
+    alert(NoRight/noQuestions)
+    questionNo = null;
   };
-  //this function appends 4 answers to the h3 elements in the html in random order the correct answer being the fourth input param 
-  function appendAnswers(answers,realAnswer){
-    console.log(answers, realAnswer)
-
+  //this function appends 4 answers to the h3 elements in the html in random order the correct answer being the fourth input param and also increases question no by 1 
+  function appendAnswers(answers){
+    answers.forEach((answer, indx)=>{
+      $(`#answer${indx}`).html(answer)
+    })
+    questionNo= questionNo + 1;
+  }
+  //this function takes in the question that you are currently on and sees whether the checkbox filled correlates with the correct answer for that question, if it does then it adds one to the score
+  function takeScore(questionNo){
+    //represents the checkbox which the user has just checked
+    let currCheckbox = $(currUserAnswer).attr("id")
+    //if the label associated with the checkbox the user has checked is the correct answer for the question defined by each case paramter then add 1 to the users score
+    switch(questionNo){
+      case 1:
+      if($(`label[for=${currCheckbox}]`).html() == artistStartYr){
+        console.log("u right")
+        playerScore += 1
+      }
+      break;
+      case 2:
+      if($(`label[for=${currCheckbox}]`).html() == artistOrigin){
+        console.log("u right")
+        playerScore += 1
+      }
+      break;
+      case 3:
+      if($(`label[for=${currCheckbox}]`).html() == artistRelease){
+        console.log("u right")
+        playerScore += 1
+      }
+      break;
+    }
   }
   //function generates an array of years within 10 years of but not including the date param, the length of the array is determined by numdates
   function generateDates(date, numDates){
@@ -113,7 +155,7 @@ function playQuiz(){
     return dates
   }
   //function generates a array of random albums, length is determined by the numAlbums input, album to avoid placing in array is specefied by the avoid album param
-  function generateAlbums(numAlbums, avoidAlbum){
+  function generateAlbums(avoidAlbum,numAlbums){
   const returnAlbums = []
   const albums = ["The Piper at the Gates of Dawn","A Saucerful of Secrets","More","Ummagumma","Atom Heart Mother","Meddle","Obscured by Clouds","The Dark Side of the Moon","Wish You Were Here","Animals","The Final Cut","Eagles","Desperado","Hotel California","The Long Run","Hell Freezes Over","Hybrid Theory","Meteora","Minutes to Midnight","A Thousand Suns","Living Things","Indicud","Satellite Flight: The Journey to Mother Moon","Speedin' Bullet 2 Heaven","Passion, Pain & Demon Slayin","Man on the Moon III: The Chosen","Day 'n' Nite","Pursuit of Happiness"]
   let i = 0
@@ -128,18 +170,18 @@ function playQuiz(){
 }
   //function generates a array of random locations, length is determined by the numLocations input
   
-  function generateLocations(numLocations){
+  function generateLocations(avoidLocation,numLocations){
     //an array of major cities in the US
     const locations = [];
     const cityNames = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
-    for(let i = 0; i<numLocations;i++){
-      locations.push(cityNames[Math.floor(Math.random() * cityNames.length)]) 
+    let i = 0;
+    while(i<numLocations){
+      let location = cityNames[Math.floor(Math.random() * cityNames.length)]
+      if (location != avoidLocation){
+        locations.push(location)
+        i++
+      } 
     }
     return locations
   }
-
-  //displays the first question then creates an event listener for the submit button within answers div 
-  $("#submitAnswer").on("click", ()=>{
-    askQuestion()
-  })
 } 
